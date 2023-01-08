f34ab527eb5d309537d9fdbf61b24ce34d12d6a8
Cocowwy <514658459@qq.com>
2023-01-08 17:28:10 +0800

    🍓 SpringUtil提供

---
+++ coco-boot-starter/src/main/java/cn/cocowwy/cocobootstarter/util/SpringUtil.java
@@ -0,0 +1,37 @@
+package cn.cocowwy.cocobootstarter.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+/**
+ * 容器工具类
+ * @author <a href="https://github.com/Cocowwy">Cocowwy</a>
+ * @since 2023/1/8
+ */
+public class SpringUtil implements ApplicationContextAware {
+    private static ApplicationContext applicationContext;
+
+    public SpringUtil() {
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringUtil.applicationContext = applicationContext;
+    }
+
+    protected static ApplicationContext getApplicationContext() {
+        return applicationContext;
+    }
+
+    public static Object getBean(String name) {
+        return getApplicationContext().getBean(name);
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return getApplicationContext().getBean(clazz);
+    }
+
+    public static <T> T getBean(String name, Class<T> clazz) {
+        return getApplicationContext().getBean(name, clazz);
+    }
+}
