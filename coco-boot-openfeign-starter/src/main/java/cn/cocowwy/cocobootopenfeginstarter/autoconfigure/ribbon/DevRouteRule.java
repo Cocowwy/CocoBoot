f96070932caf530f1d24888269d9f10d147ceba8
Cocowwy <514658459@qq.com>
2023-02-15 12:34:10 +0800

    checkstyle

--- coco-boot-openfeign-starter/src/main/java/cn/cocowwy/cocobootopenfeginstarter/autoconfigure/ribbon/DevRouteRule.java
+++ coco-boot-openfeign-starter/src/main/java/cn/cocowwy/cocobootopenfeginstarter/autoconfigure/ribbon/DevRouteRule.java
@@ -7,10 +7,7 @@ import com.netflix.loadbalancer.PredicateBasedRule;
 import com.netflix.loadbalancer.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.util.StringUtils;
 
-import java.net.URI;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
